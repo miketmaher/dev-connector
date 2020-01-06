5b774ccc18c609604c9908b703fb45c191a37288
mike-maher <mike.maher@aqmetrics.com>
2020-01-06 09:53:04 +0000

    [UPDATE] added login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
 const auth = require('../middleware/auth');
 const User = require('../models/User');
 
 /**
  * @route  GET /auth
- * @desc   Test route
+ * @desc   get user based on token
  * @access PRIVATE
  **/
 router.get('/', auth, async (req, res) => {
@@ -16,5 +20,66 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
+/**
+ * @route  POST /auth/login
+ * @desc   log in User
+ * @access PUBLIC
+ **/
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        },
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal server error');
+    }
+  },
+);
 
 module.exports = router;
