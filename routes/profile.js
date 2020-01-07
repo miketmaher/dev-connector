859417eeee5ee05bcc7037e1907dc544de7eb991
mike-maher <mike.maher@aqmetrics.com>
2020-01-07 14:15:24 +0000

    [UPDATE] adde delete experience route

--- routes/profile.js
+++ routes/profile.js
@@ -187,4 +187,82 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+/**
+ * @route  PUT /profile/experience
+ * @desc   Add profile experience
+ * @access PRIVATE
+ **/
+
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal server error');
+    }
+  },
+);
+
+/**
+ * @route  DELETE /profile/experience/:id
+ * @desc   delete profile experience
+ * @access PRIVATE
+ **/
+
+router.delete('/experience/:id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const index = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.id);
+    profile.experience.splice(index, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal server error');
+  }
+});
+
 module.exports = router;
