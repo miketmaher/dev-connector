2380b1f83febc654bbffecc0289234a73274a793
mike-maher <mike.maher@aqmetrics.com>
2020-01-07 16:12:31 +0000

    [UPDATE] Added get all, add and delete post routes

--- routes/posts.js
+++ routes/posts.js
@@ -43,4 +43,68 @@ router.post(
   },
 );
 
+/**
+ * @route  GET /posts
+ * @desc   get all posts
+ * @access PRIVATE
+ **/
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal server error');
+  }
+});
+
+/**
+ * @route  GET /posts/:id
+ * @desc   get post by id
+ * @access PRIVATE
+ **/
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Internal server error');
+  }
+});
+
+/**
+ * @route  DELETE /posts/:id
+ * @desc   delete post
+ * @access PRIVATE
+ **/
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ msg: 'Post not found' });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await post.remove();
+    res.json({ msg: 'Post deleted' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Internal server error');
+  }
+});
+
 module.exports = router;
