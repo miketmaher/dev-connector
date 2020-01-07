8fea60b1d5377544e63aa6f7ad62e95638922145
mike-maher <mike.maher@aqmetrics.com>
2020-01-07 11:21:14 +0000

    [UPDATE] added profile model and  get profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+const auth = require('../middleware/auth');
+const Profile = require('../models/Profile');
+const User = require('../models/User');
 
 /**
- * @route  GET /profile
- * @desc   Test route
- * @access PUBLIC
+ * @route  GET /profile/me
+ * @desc   get current user's profile
+ * @access PRIVATE
  **/
 
-router.get('/', (req, res) => res.send('profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'No profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal server error');
+  }
+});
 
 module.exports = router;
