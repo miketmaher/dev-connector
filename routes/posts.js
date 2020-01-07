1ada236fff808a334aa3fa6acb876e369ec5ffe7
mike-maher <mike.maher@aqmetrics.com>
2020-01-07 15:57:54 +0000

    [UPDATE] added create post route

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,46 @@
 const router = require('express').Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Post = require('../models/Post');
+const User = require('../models/User');
+const Profile = require('../models/Profile');
 
 /**
- * @route  GET /posts
- * @desc   Test route
- * @access PUBLIC
+ * @route  POST /posts
+ * @desc   create a post
+ * @access PRIVATE
  **/
 
-router.get('/', (req, res) => res.send('posts route'));
+router.post(
+  '/',
+  [
+    auth,
+    check('text', 'Text is required')
+      .not()
+      .isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const { name, avatar } = user;
+      const newPost = new Post({
+        text: req.body.text,
+        user: req.user.id,
+        name,
+        avatar,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal server error');
+    }
+  },
+);
 
 module.exports = router;
