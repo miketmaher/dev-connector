77041527e576f62ba43cea0d0efad58ad39aa62a
mike-maher <mike.maher@aqmetrics.com>
2020-01-14 10:11:45 +0000

    [UPDATE] added logout action

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../redux/actions/authActions';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const auth = useSelector(state => state.auth);
+  const { isAuthenticated, loading } = auth;
+  const dispatch = useDispatch();
+  const authorizedLinks = (
+    <ul>
+      <li>
+        <a onClick={() => dispatch(logout())} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
   return (
     <nav className="navbar bg-dark">
       <h1>
@@ -9,17 +37,9 @@ const Navbar = () => {
           <i className="fas fa-code"></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="/profiles">Developers</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authorizedLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 import setAuthToken from '../../utils/setAuthToken';
 
@@ -73,4 +74,8 @@ export const loginUser = ({ email, password }) => async dispatch => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
